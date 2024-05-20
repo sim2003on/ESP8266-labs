026db83ede8dfb45ff0202c0458f6c7a7e6ed74b
Simon Fereshetyan <57359168+sim2003on@users.noreply.github.com>
2024-05-20 23:47:51 +0400

    Update code7.ino

--- code7.ino
+++ code7.ino
@@ -14,10 +14,10 @@
 // For the following credentials, see examples/Authentications/SignInAsUser/EmailPassword/EmailPassword.ino
 
 /* 2. Define the API Key */
-#define API_KEY "AIzaSyD8ZdUI1l6KwwsNJKzYkaVeLIxHChFIPgc"
+#define API_KEY ""
 
 /* 3. Define the RTDB URL */
-#define DATABASE_URL "https://esp-firebase-demo-saten-default-rtdb.europe-west1.firebasedatabase.app/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+#define DATABASE_URL "https://esp-firebase-demo-default-rtdb.europe-west1.firebasedatabase.app/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
 
 /* 4. Define the user Email and password that alreadey registerd or added in your project */
 #define USER_EMAIL "inform.k12.1@gmail.com"
